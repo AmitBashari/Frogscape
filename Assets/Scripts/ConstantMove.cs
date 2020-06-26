83b65c40a1a85a5e78db9c2e50109919abd1082b
AmitBashari <bashari.amit@gmail.com>
2020-06-26 15:41:40 +0300

    Added forward movement to player and camera

---
+++ Assets/Scripts/ConstantMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantMove : MonoBehaviour
+{
+    public Vector3 Speed; // C Sharp convention to use a capital 'S' since this variable is public
+
+    private void Update() 
+    {
+        transform.Translate(Speed * Time.deltaTime, Space.Self);
+    }
+}
